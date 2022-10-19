9fe14998236d6afb3a1578afbe013b274f701149
victor A <ajorvictor48@gmail.com>
2022-10-19 15:24:16 -0700

    sign numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * prints the sign of a number.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+	return (-1);
+}
+
